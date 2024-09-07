4f99355214bb7830e6c01714f456598fc8c239b3
BartekWons <bartek.wons@gmail.com>
2024-09-07 15:12:46 +0200

    add menubar

---
+++ Investify/Investify/MVVM/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,30 @@
+﻿using Investify.Core;
+using System.Windows;
+
+namespace Investify.MVVM.ViewModel
+{
+    public class MainWindowViewModel
+    {
+        public RelayCommand CloseCommand { get; set; }
+        public RelayCommand MinimizeCommand { get; set; }
+        public RelayCommand DragWindowCommand { get; set; }
+
+        public MainWindowViewModel()
+        {
+            CloseCommand = new RelayCommand(o =>
+            {
+                Application.Current.MainWindow.Close();
+            });
+
+            MinimizeCommand = new RelayCommand(o =>
+            {
+                Application.Current.MainWindow.WindowState = WindowState.Minimized;
+            });
+
+            DragWindowCommand = new RelayCommand(o =>
+            {
+                Application.Current.MainWindow.DragMove();
+            });
+        }
+    }
+}
